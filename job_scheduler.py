6cef35e9b6bdc4406fe3eec23f59e0b5cc157ef8
may <maykhaleghi@gmail.com>
2023-12-05 16:11:04 +0330

    changed data using pandas, created FIFO function

--- job_scheduler.py
+++ job_scheduler.py
@@ -1,28 +1,42 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import seaborn as sns
 sns.set()
 
-tasks = ["task1", "task2", "task3"]
-CBT = [3, 2, 5]
+df = pd.DataFrame(
+    {
+        "tasks": ["task1", "task2", "task3"],
+        "AT": [3, 0, 2],
+        "CBT": [3, 2, 1],
+    }
+)
 
-
-def function(tasks, CBT):
+def function(df):
+    p = df.sort_values("AT")
     start = []
     end = []
     var = 0
-    for i in CBT:
+    for i in p["tasks"]:
         start.append(var)
-        var = var + i
+        print("var", var)
+        
+        x = p.query("tasks == @i")["CBT"].astype('int')
+        print("varrrr", type(x))
+        var = var + x
+        #if var >= (p.query("tasks == @i")["AT"]):
+            #var = p.query("tasks == @i")["AT"]
+        #print("i", i)
         end.append(var)
-    print(start)
-    print(end)
+    #print(start)
+    #print(end)
+
 
     fig, gnt = plt.subplots(figsize=(10, 6))
 
-    gantt = gnt.barh(tasks, CBT, left = start)
+    gantt = gnt.barh(p["tasks"], p["CBT"], left = start)
     #gantt = gnt.bar(x = tasks , height = CBT)
     gnt.bar_label(gantt, end, padding = -17, color = "white")
     plt.show()
 
-function(tasks, CBT)
+function(df)
